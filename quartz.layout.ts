03d8a1b5ae4d94e5d6f2c5ce680391c135187bdc
JanC <108794854+janc-pxl@users.noreply.github.com>
2026-03-31 17:36:50 +0200

    Comment out left layout components
    Comment out the left layout components in quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  left: [
+  // left: [
     // Component.PageTitle(),
     // Component.MobileOnly(Component.Spacer()),
     // Component.Flex({
@@ -37,7 +37,7 @@ export const defaultContentPageLayout: PageLayout = {
     //   ],
     // }),
     // Component.Explorer(),
-  ],
+  // ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),

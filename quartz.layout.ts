8ebb97ea83488b09f5d38eced9fe14c14e57831a
JanC <108794854+janc-pxl@users.noreply.github.com>
2026-03-31 17:33:04 +0200

    Disable left column components in quartz layout
    Comment out unused components in the layout.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,19 +24,19 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
+    // Component.PageTitle(),
+    // Component.MobileOnly(Component.Spacer()),
+    // Component.Flex({
+    //   components: [
+    //     {
+    //       Component: Component.Search(),
+    //       grow: true,
+    //     },
+    //     { Component: Component.Darkmode() },
+    //     { Component: Component.ReaderMode() },
+    //   ],
+    // }),
+    // Component.Explorer(),
   ],
   right: [
     Component.Graph(),

f72f4a9e2596846fb68d5027fef6f4221697d624
Jan Castermans <jan.castermans@pxl.be>
2026-04-01 08:32:06 +0200

    Quartz sync: Apr 1, 2026, 8:32 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,21 +23,21 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  // left: [
-    // Component.PageTitle(),
-    // Component.MobileOnly(Component.Spacer()),
-    // Component.Flex({
-    //   components: [
-    //     {
-    //       Component: Component.Search(),
-    //       grow: true,
-    //     },
-    //     { Component: Component.Darkmode() },
-    //     { Component: Component.ReaderMode() },
-    //   ],
-    // }),
-    // Component.Explorer(),
-  // ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.Explorer(),
+  ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
